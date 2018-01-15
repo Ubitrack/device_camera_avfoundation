907d3399486d3b4fe445fbf76cedf1f45b842e3f
Ulrich Eck <ueck@net-labs.de>
2018-01-15 18:00:30 +0100

    initial conan build setup

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conans import ConanFile, CMake, tools
+
+
+class UbitrackCoreConan(ConanFile):
+    name = "ubitrack_device_camera_avfoundation"
+    version = "1.3.0"
+
+    description = "Ubitrack Device Camera OSX AV Foundation"
+    url = "https://github.com/Ubitrack/device_camera_avfoundation.git"
+    license = "GPL"
+
+    short_paths = True
+    settings = "os", "compiler", "build_type", "arch"
+
+    generators = "cmake"
+
+    requires = (
+        "ubitrack_core/%s@ubitrack/stable" % version,
+        "ubitrack_vision/%s@ubitrack/stable" % version,
+        "ubitrack_dataflow/%s@ubitrack/stable" % version,
+       )
+
+    default_options = (
+        "ubitrack_core:shared=True",
+        "ubitrack_vision:shared=True",
+        "ubitrack_dataflow:shared=True",
+        )
+
+    # all sources are deployed with the package
+    exports_sources = "doc/*", "src/*", "CMakeLists.txt"
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin") # From bin to bin
+        self.copy(pattern="*.dylib*", dst="lib", src="lib") 
+        self.copy(pattern="*.so*", dst="lib", src="lib") 
+       
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        pass
